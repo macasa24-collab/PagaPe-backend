22809e86d50ef6eac7b67b0976f455da43f435d5
keacma24-a11y <keacma24@bemen3.cat>
2026-04-09 19:48:20 +0200

    Creando metodos hibernate en las clases 1/8

--- src/main/java/com/pagape/api/model/Usuario.java
+++ src/main/java/com/pagape/api/model/Usuario.java
@@ -2,8 +2,16 @@ package com.pagape.api.model;
 
 import java.util.ArrayList;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String email;
