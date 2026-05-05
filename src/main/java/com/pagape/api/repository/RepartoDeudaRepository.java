b304d6715c204b5514c6ff93dd0f40ee1469125c
darolo24-star <darolo24@bemen3.cat>
2026-05-05 19:19:12 +0200

    Improvement: Se añade logica de division de gasto sin personalizar

--- src/main/java/com/pagape/api/repository/RepartoDeudaRepository.java
+++ src/main/java/com/pagape/api/repository/RepartoDeudaRepository.java
@@ -1,8 +1,11 @@
 package com.pagape.api.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.pagape.api.model.RepartoDeuda;
@@ -15,5 +18,11 @@ public interface RepartoDeudaRepository extends JpaRepository<RepartoDeuda, Repa
 
     // Método para encontrar deudas de un usuario en un gasto específico
     RepartoDeuda findById_IdGastoAndId_IdUsuarioDeudor(Integer idGasto, Integer idUsuarioDeudor);
+
+    @Query("SELECT COALESCE(SUM(r.cuotaDebe), 0) FROM RepartoDeuda r JOIN Gasto g ON r.id.idGasto = g.id WHERE g.pagador.id = :userId AND g.planOrigen.grupo.id = :grupoId")
+    BigDecimal sumCuotaDebeWherePagadorIs(@Param("userId") Integer userId, @Param("grupoId") Integer grupoId);
+
+    @Query("SELECT COALESCE(SUM(r.cuotaDebe), 0) FROM RepartoDeuda r JOIN Gasto g ON r.id.idGasto = g.id WHERE r.id.idUsuarioDeudor = :userId AND g.planOrigen.grupo.id = :grupoId")
+    BigDecimal sumCuotaDebeWhereDeudorIs(@Param("userId") Integer userId, @Param("grupoId") Integer grupoId);
 
 }
