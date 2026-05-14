a82a45461e29677f58927cb0e63a4c2036c7bb82
keacma24-a11y <keacma24@bemen3.cat>
2026-05-14 19:23:49 +0200

    cambio mejora

--- src/main/java/com/pagape/api/security/SecurityConfig.java
+++ src/main/java/com/pagape/api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pagape.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
